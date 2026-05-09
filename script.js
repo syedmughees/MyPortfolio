acfb1da6a44ea56f393236d8657dd0352b16b28d
syedmughees <37158320+syedmughees@users.noreply.github.com>
2026-05-10 04:58:35 +0500

    updated .typing text

--- script.js
+++ script.js
@@ -37,11 +37,12 @@ $(document).ready(function(){
     if ($(".typing").length) {
         var typed = new Typed(".typing", {
             strings: [
-                "OJS Consultant",
-                "Crossref Ambassador",
-                "Journal Publishing Specialist",
-                "OJS Plugin Developer",
-                "Publishing Workflow Consultant",
+                "OJS Publishing Consultant",
+                "Crossref & DOI Integration Specialist",
+                "Crossmark Integration Specialist",
+                "Academic Journal Systems Expert",
+                "OJS Plugin & Workflow Developer",
+                "Journal Migration Consultant",
                 "Academic Journal Trainer"
             ],
             typeSpeed: 100,
